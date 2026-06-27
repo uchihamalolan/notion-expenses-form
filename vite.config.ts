75c827e681b9621cf6a63494aa341d226a22c066
Malolan B <ma101an@icloud.com>
2026-06-28 00:23:39 +0530

    update scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import tailwindcss from "@tailwindcss/vite";
 import adapter from "@sveltejs/adapter-auto";
 import { sveltekit } from "@sveltejs/kit/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
